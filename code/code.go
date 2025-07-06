a1136f11173de34e6a1e699b2f85b65b76acd283
Jeffvo <jeffrey.oosterwijk@hotmail.com>
2025-07-06 20:42:02 +0100

    Adding on the Stack

--- code/code.go
+++ code/code.go
@@ -36,18 +36,23 @@ func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
 	if len(operands) != operandCount {
 		return fmt.Sprintf("ERROR: operand len %d does not match defined %d\n", len(operands), operandCount)
 	}
+
 	switch operandCount {
+	case 0:
+		return def.Name
 	case 1:
 		return fmt.Sprintf("%s %d", def.Name, operands[0])
 	}
 
 	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
+
 }
 
 type Opcode byte
 
 const (
 	OpConstant Opcode = iota
+	OpAdd
 )
 
 type Definition struct {
@@ -58,6 +63,8 @@ type Definition struct {
 var definitions = map[Opcode]*Definition{
 	OpConstant: {
 		"OpConstant", []int{2}},
+	OpAdd: {
+		"OpAdd", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,6 +1,8 @@
 package compiler
 
 import (
+	"fmt"
+
 	"github.com/jeffvo/go-monkey-compiler/ast"
 	"github.com/jeffvo/go-monkey-compiler/code"
 	"github.com/jeffvo/go-monkey-compiler/object"
@@ -41,6 +43,14 @@ func (c *Compiler) Compile(node ast.Node) error {
 		err = c.Compile(node.Right)
 		if err != nil {
 			return err
+		}
+
+		switch node.Operator {
+		case "+":
+			c.emit(code.OpAdd)
+		default:
+			return fmt.Errorf("unknown operator: %s", node.Operator)
+
 		}
 
 	case *ast.IntegerLiteral:
