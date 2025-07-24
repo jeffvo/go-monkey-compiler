5096cd9c07e23199bb07d368a9e08834bb879e27
Jeffvo <jeffrey.oosterwijk@hotmail.com>
2025-07-24 20:53:17 +0100

    Adding Globals to the VM

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/jeffvo/go-monkey-compiler/compiler"
 	"github.com/jeffvo/go-monkey-compiler/lexer"
+	"github.com/jeffvo/go-monkey-compiler/object"
 	"github.com/jeffvo/go-monkey-compiler/parser"
 	"github.com/jeffvo/go-monkey-compiler/vm"
 )
@@ -15,6 +16,10 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+
+	constants := []object.Object{}
+	globals := make([]object.Object, vm.GlobalsSize)
+	symbolTable := compiler.NewSymbolTable()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -33,14 +38,17 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		comp := compiler.New()
+		comp := compiler.NewWithState(symbolTable, constants)
 		err := comp.Compile(program)
 		if err != nil {
 			fmt.Fprintf(out, "Woops! Compilation error: %s\n", err)
 			continue
 		}
 
-		machine := vm.New(comp.Bytecode())
+		code := comp.Bytecode()
+		constants = code.Constants
+		machine := vm.NewWithGlobalsStore(code, globals)
+
 		err = machine.Run()
 		if err != nil {
 			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
